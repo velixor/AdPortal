a599d397ecc05d2283064a668d61dee93066a530
velixor <velixor.work@gmail.com>
2019-10-14 21:59:49 +0300

    add validation for AddRequest

---
+++ AdPortalApi/Validators/AdRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using Dto.Contracts.AdContracts;
+using FluentValidation;
+
+namespace AdPortalApi.Validators
+{
+    public class AdRequestValidator : AbstractValidator<AdRequest>
+    {
+        public AdRequestValidator()
+        {
+            RuleFor(ad => ad.Content).NotEmpty();
+            RuleFor(ad => ad.UserId).NotNull();
+        }
+    }
+}
