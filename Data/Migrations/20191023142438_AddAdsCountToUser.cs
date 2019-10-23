b85e5f753863ea873e1e8d89f0f09ba0c1be93dd
velixor <velixor.work@gmail.com>
2019-10-23 20:38:09 +0300

    db update

---
+++ Data/Migrations/20191023142438_AddAdsCountToUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class AddAdsCountToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AdsCount",
+                table: "Users",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AdsCount",
+                table: "Users");
+        }
+    }
+}
